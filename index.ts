5b6f15f1dba24541927200947359171d5f3b6fdd
Fatema Freezzy <fatemafreezzy@gmail.com>
2025-08-17 21:16:28 +0530

    wokflow update2

--- index.ts
+++ index.ts
@@ -60,7 +60,7 @@ await app.register(prismaPlugin);
 await app.register(jwtPlugin);
 
 // Health + OpenAPI JSON
-app.get("/health", async () => ({ status: "ok" }));
+app.get("/health", async () => ({ status: "okk" }));
 app.get("/openapi.json", async () => app.swagger());
 
 // Routes
